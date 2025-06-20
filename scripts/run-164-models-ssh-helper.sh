f7e75aff507f5043c055aaf0a9c70c20d9d1336c
Adam Knapp <adam.knapp@instanton.org>
2025-06-20 16:11:40 -0400

    script fix

--- scripts/run-164-models-ssh-helper.sh
+++ scripts/run-164-models-ssh-helper.sh
@@ -8,13 +8,13 @@ PARENT_DIR=~/pruning/data
 SEQ_ERR=0.00
 ADO=0.00
 
-NSITES="$0"
-printf -v SEQ "%03d" "$1"
+NSITES="$1"
+printf -v IDX "%03d" "$2"
 
 pruning \
-  --seqs "$PARENT_DIR"/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/diploid-"$SEQ".phy \
-  --tree "$PARENT_DIR"/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/tree-"$SEQ".nwk \
-  --output "$PARENT_DIR"/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/reconstructed-tree-phased_dna16_4-"$SEQ" \
+  --seqs "$PARENT_DIR"/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/diploid-"$IDX".phy \
+  --tree "$PARENT_DIR"/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/tree-"$IDX".nwk \
+  --output "$PARENT_DIR"/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/reconstructed-tree-phased_dna16_4-"$IDX" \
   --model PHASED_DNA16_4 \
   --log
 
