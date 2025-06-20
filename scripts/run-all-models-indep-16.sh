b114140c7c98bd2e760f05bc789aef6453e06bf2
Adam Knapp <adam.knapp@instanton.org>
2025-06-20 14:19:27 -0400

    script cleanup

--- scripts/run-all-models-indep-16.sh
+++ scripts/run-all-models-indep-16.sh
@@ -4,31 +4,30 @@ cd ~/pruning/ || exit
 
 source venv/bin/activate
 
-NJOBS=$(nproc --ignore 2)
-
+PARENT_DIR=~/pruning/data
 SEQ_ERR=0.00
 ADO=0.00
+
+NJOBS=$(nproc --ignore 2)
+
 for NSITES in 1000 10000
 do
   seq -w 0 99 | parallel --line-buffer --jobs "$NJOBS" -I {} \
     pruning \
-      --seqs ~/pruning/data/temp/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/diploid-0{}.phy \
-      --tree ~/pruning/data/temp/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/tree-0{}.nwk \
-      --output ~/pruning/data/temp/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/reconstructed-tree-phased16-0{} \
+      --seqs "$PARENT_DIR"/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/diploid-0{}.phy \
+      --tree "$PARENT_DIR"/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/tree-0{}.nwk \
+      --output "$PARENT_DIR"/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/reconstructed-tree-phased16-0{} \
       --model PHASED_DNA16 \
       --log
 done
 
-
-SEQ_ERR=0.00
-ADO=0.00
 for NSITES in 1000 10000
 do
   seq -w 0 99 | parallel --line-buffer --jobs "$NJOBS" -I {} \
     pruning \
-      --seqs ~/pruning/data/temp/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/diploid-0{}.phy \
-      --tree ~/pruning/data/temp/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/tree-0{}.nwk \
-      --output ~/pruning/data/temp/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/reconstructed-tree-phased16mp-0{} \
+      --seqs "$PARENT_DIR"/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/diploid-0{}.phy \
+      --tree "$PARENT_DIR"/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/tree-0{}.nwk \
+      --output "$PARENT_DIR"/diploid-sites-"$NSITES"-seq-err-"$SEQ_ERR"-ado-"$ADO"/reconstructed-tree-phased16mp-0{} \
       --model PHASED_DNA16_MP \
       --log
 done
